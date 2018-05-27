2afc4029d96e875a93881054f057f067296c95ba
TheMich4 <themich4@gmail.com>
2018-05-27 12:48:13 +0200

    Cells now have random colours.

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
 class CategoryViewController: SwipeTableViewController {
     
@@ -21,6 +22,7 @@ class CategoryViewController: SwipeTableViewController {
          loadCategories()
         
         tableView.rowHeight = 65.0
+        tableView.separatorStyle = .none
     }
 
     // MARK: - TableView Datasource Methods
@@ -36,6 +38,8 @@ class CategoryViewController: SwipeTableViewController {
         let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories Added Yet"
+        
+        cell.backgroundColor = UIColor(hexString: (categories?[indexPath.row].color)!)
         
         return cell
     }
